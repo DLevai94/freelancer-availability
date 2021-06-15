dd876bffce07138ae0fa69519c13fecfef186abc
David Levai <david@davidlevai.com>
2021-06-15 23:08:33 +0200

    refactor

--- pages/api/monthly-mail.ts
+++ pages/api/monthly-mail.ts
@@ -14,39 +14,21 @@ type ResData = {
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Cache-Control', 'no-store, max-age=0');
   if (req?.headers?.authorization?.split(' ')?.[1] !== `${process?.env?.AUTH_SECRET}`) {
-    res.status(401).send({ result: 'Authorization error' });
-    return;
+    return res.status(401).json({ result: 'Authorization error' });
   }
-  console.log('start');
 
   try {
     const records = await table.select({ fields: ['First Name', 'Email'] });
-
+    let users = [];
     records.eachPage(
       async function page(records, fetchNextPage) {
-        console.log('foreach');
         for await (const record of records) {
-          console.log('Retrieved', record.get('First Name'));
-          console.log('Retrieved', record.get('Email'));
+          console.log('foreach');
+          const firstName = record.get('First Name');
+          const email = record.get('Email');
           const id = record.getId();
-          await mailClient
-            .sendEmailWithTemplate({
-              From: process.env.POSTMARK_FROM,
-              To: 'david.levai@screamingbox.com',
-              TemplateAlias: 'monthly-availability',
-              TemplateModel: {
-                name: record.get('First Name'),
-                month: 'June',
-                action_url_0: `${process.env.VERCEL_URL}/message?user=${id}&availability=0`,
-                action_url_10: `${process.env.VERCEL_URL}/message?user=${id}&availability=10`,
-                action_url_20: `${process.env.VERCEL_URL}/message?user=${id}&availability=20`,
-                action_url_30: `${process.env.VERCEL_URL}/message?user=${id}&availability=30`,
-                action_url_40: `${process.env.VERCEL_URL}/message?user=${id}&availability=40`,
-              },
-              MessageStream: 'monthly-availability',
-            })
-            .then((res) => console.log('sent'))
-            .catch((err) => console.log(err));
+          users = [...users, { id, email, firstName }];
+          console.log(`users: ${users}`);
         }
 
         fetchNextPage();
@@ -54,16 +36,39 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       function done(err) {
         if (err) {
           console.error(err);
-          res.status(500).send({ result: 'Error' });
-          return;
+          return res.status(500).json({ result: 'Error' });
         }
-        res.status(200).json({ result: 'ok' });
-        return;
+        Promise.all(
+          users.map(async (freelancer) => {
+            await mailClient.sendEmailWithTemplate({
+              From: process.env.POSTMARK_FROM,
+              To: 'david.levai@screamingbox.com',
+              TemplateAlias: 'monthly-availability',
+              TemplateModel: {
+                name: freelancer.firstName,
+                month: 'June',
+                action_url_0: `${process.env.VERCEL_URL}/message?user=${freelancer.id}&availability=0`,
+                action_url_10: `${process.env.VERCEL_URL}/message?user=${freelancer.id}&availability=10`,
+                action_url_20: `${process.env.VERCEL_URL}/message?user=${freelancer.id}&availability=20`,
+                action_url_30: `${process.env.VERCEL_URL}/message?user=${freelancer.id}&availability=30`,
+                action_url_40: `${process.env.VERCEL_URL}/message?user=${freelancer.id}&availability=40`,
+              },
+              MessageStream: 'monthly-availability',
+            });
+          })
+        )
+          .then(() => {
+            console.log('sent all');
+            return res.status(200).json({ result: 'ok' });
+          })
+          .catch((error) => {
+            console.log(`error: ${error}`);
+            return res.status(200).json({ result: 'error', error });
+          });
       }
     );
   } catch (error) {
-    console.log('error');
-    console.log(error);
-    res.status(200).json({ result: 'error', error });
+    console.log(`error: ${error}`);
+    return res.status(200).json({ result: 'error', error });
   }
 };
